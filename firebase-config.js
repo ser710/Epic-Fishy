eca1fa39d1d69e16754cf5bfb2acb8901cbabf28
ser710 <70785424+ser710@users.noreply.github.com>
2026-07-14 17:14:57 -0400

    Refactor Firebase configuration to export object
    Export the firebaseConfig object for use in other modules.

--- firebase-config.js
+++ firebase-config.js
@@ -5,51 +5,11 @@
 // Until you replace the placeholder apiKey below, Community features stay
 // switched off and the rest of the app works exactly as before.
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC69WWiIgBsvXlfHTuXdAu8o7Z1rz3gKR4",
-    authDomain: "epic-fishy-b4195.firebaseapp.com",
-    projectId: "epic-fishy-b4195",
-    storageBucket: "epic-fishy-b4195.firebasestorage.app",
-    messagingSenderId: "249068647390",
-    appId: "1:249068647390:web:f2d972c7947c683381e34f"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>{
-  "project_info": {
-    "project_number": "362455123000",
-    "firebase_url": "https://epic-fishy-default-rtdb.firebaseio.com",
-    "project_id": "epic-fishy",
-    "storage_bucket": "epic-fishy.firebasestorage.app"
-  },
-  "client": [
-    {
-      "client_info": {
-        "mobilesdk_app_id": "1:362455123000:android:c69b60dba1d98af38112cd",
-        "android_client_info": {
-          "package_name": "Epic.Fishy"
-        }
-      },
-      "oauth_client": [],
-      "api_key": [
-        {
-          "current_key": "AIzaSyAzOaIiQcz-n5X5Hv3zlETIyccVJPrx87E"
-        }
-      ],
-      "services": {
-        "appinvite_service": {
-          "other_platform_oauth_client": []
-        }
-      }
-    }
-  ],
-  "configuration_version": "1"
-}
+export const firebaseConfig = {
+  apiKey: "AIzaSyC69WWiIgBsvXlfHTuXdAu8o7Z1rz3gKR4",
+  authDomain: "epic-fishy-b4195.firebaseapp.com",
+  projectId: "epic-fishy-b4195",
+  storageBucket: "epic-fishy-b4195.firebasestorage.app",
+  messagingSenderId: "249068647390",
+  appId: "1:249068647390:web:f2d972c7947c683381e34f",
+};
