664f181b638e696979c8b5c7fe5f4e6c7182a86e
ser710 <70785424+ser710@users.noreply.github.com>
2026-07-14 17:11:26 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,25 @@
 // Until you replace the placeholder apiKey below, Community features stay
 // switched off and the rest of the app works exactly as before.
 
-{
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC69WWiIgBsvXlfHTuXdAu8o7Z1rz3gKR4",
+    authDomain: "epic-fishy-b4195.firebaseapp.com",
+    projectId: "epic-fishy-b4195",
+    storageBucket: "epic-fishy-b4195.firebasestorage.app",
+    messagingSenderId: "249068647390",
+    appId: "1:249068647390:web:f2d972c7947c683381e34f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>{
   "project_info": {
     "project_number": "362455123000",
     "firebase_url": "https://epic-fishy-default-rtdb.firebaseio.com",
