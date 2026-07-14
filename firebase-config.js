c0ee5152532d82f5527d7c5679b2c74ec4c33875
ser710 <70785424+ser710@users.noreply.github.com>
2026-07-14 16:07:55 -0400

    Enhance firebaseConfig with project and client info
    Updated firebaseConfig to include project information and client details.

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,32 @@
 // switched off and the rest of the app works exactly as before.
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
+  "project_info": {
+    "project_number": "362455123000",
+    "firebase_url": "https://epic-fishy-default-rtdb.firebaseio.com",
+    "project_id": "epic-fishy",
+    "storage_bucket": "epic-fishy.firebasestorage.app"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:362455123000:android:c69b60dba1d98af38112cd",
+        "android_client_info": {
+          "package_name": "Epic.Fishy"
+        }
+      },
+      "oauth_client": [],
+      "api_key": [
+        {
+          "current_key": "AIzaSyAzOaIiQcz-n5X5Hv3zlETIyccVJPrx87E"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": []
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
